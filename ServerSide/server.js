324bf64097c4b3e57d0426e1f9cf67fbcd0b6c80
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-08-21 01:08:42 +0530

    successfully fetched and displayed Coordinators List

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -9,7 +9,8 @@ const clientSidePath = path.join(__dirname, '..', 'ClientSide'); //Specifying th
 app.use(express.static(clientSidePath)) //the public folder is what is visible to the client (actually a subset of that folder (depending on the currently rendered webpage and it's used resources))
 app.use(express.json({limit : '1mb'} )); //telling that my app will be sending/recieving data in json format (limiting to 1MB)
 require('dotenv').config() //including the .env file (for the API keys and DB Credentials)
-const {writeDB,readDB,updateDB,deleteDB} = require("./MongoOperations.js") //including the MongoOperations.js file (for the DB operations)
+const {writeDB,readDB,updateDB,deleteDB} = require("./MongoOperations.js"); //including the MongoOperations.js file (for the DB operations)
+const { read } = require('fs');
 
 //------------------------------------------------------------------------------------------------------------------------------
 
@@ -18,38 +19,47 @@ app.listen(port, () => {
     console.log("Server Started")    
 });
 
-app.post("/create", (req, res) => {
-    writeDB("Main", "Coordinators", {"name": "Ananya", "Age" : 20}).then((acknowledged) => {
-        res.json(acknowledged)
-    }).catch((err) => {
-        console.log("Cant' Write to DB");
-        res.status(400).send("Cant' Write to DB");
-    })
-})
+// app.post("/create", (req, res) => {
+//     console.log(req.body);
+//     writeDB("Main", "Coordinators",req.body).then((acknowledged) => {
+//         res.json(acknowledged)
+//     }).catch((err) => {
+//         console.log("Cant' Write to DB");
+//         res.status(400).send("Cant' Write to DB");
+//     })
+// })
 
-app.get("/read", (req, res) => {
+// app.get("/read", (req, res) => {
+//     readDB("Main", "Coordinators", {}).then((coordinators) => {
+//         res.json(coordinators);
+//       }).catch((err) => {
+//         console.log("Cant' Read DB");
+//         res.status(400).send("Cant' Read DB");
+//       })
+// })
+
+// app.put("/update", (req, res) => {
+//     updateDB("Main", "Coordinators", {"name": "Ananya"}, {"name": "Ananya", "Age" : 31}).then((acknowledged) => {
+//         res.json(acknowledged);
+//     }).catch((err) => {
+//         console.log("Cant' Update DB");
+//         res.status(400).send("Cant' Update DB");
+//     })
+// })
+
+// app.delete("/delete", (req, res) => {
+//     deleteDB("Main", "Coordinators", {"name": "Ananya"}).then((acknowledged) => {
+//         res.json(acknowledged);
+//     }).catch((err) => {
+//         console.log("Cant' Delete from DB");
+//         res.status(400).send("Cant' Delete from DB");
+//     })
+// })
+
+app.get("/coordinatorsList",(req,res) => {
     readDB("Main", "Coordinators", {}).then((coordinators) => {
         res.json(coordinators);
       }).catch((err) => {
-        console.log("Cant' Read DB");
         res.status(400).send("Cant' Read DB");
       })
-})
-
-app.put("/update", (req, res) => {
-    updateDB("Main", "Coordinators", {"name": "Ananya"}, {"name": "Ananya", "Age" : 31}).then((acknowledged) => {
-        res.json(acknowledged);
-    }).catch((err) => {
-        console.log("Cant' Update DB");
-        res.status(400).send("Cant' Update DB");
-    })
-})
-
-app.delete("/delete", (req, res) => {
-    deleteDB("Main", "Coordinators", {"name": "Ananya"}).then((acknowledged) => {
-        res.json(acknowledged);
-    }).catch((err) => {
-        console.log("Cant' Delete from DB");
-        res.status(400).send("Cant' Delete from DB");
-    })
 })
