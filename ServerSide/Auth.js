f614f5d31a918acb8c6b48b87f52d97728e2b8bd
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-08-28 02:05:03 +0530

    successfully added Google OAuth login

---
+++ ServerSide/Auth.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  console.log("called 1");
+  // console.log(profile);
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => { //To determine what data should be stored in the session. 
+  console.log("called 2");
+  // console.log(user);
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => { //to retrive full user data based on the stored identifier
+  console.log("called 3");
+  // console.log(user);
+  done(null, user);
+});
+
+module.exports = passport;
--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,36 +1,80 @@
 //------------------------------------------------------------Node Packages-----------------------------------------------------
-//Path
-const path = require('path');
-//EXPRESS
 const express = require("express"); //including express package for creating a server
+const session = require('express-session'); // Import express-session use to manage sessions
+const path = require('path');
+require('dotenv').config() //including the .env file (for the API keys and DB Credentials)
+
 const app = express();
-const port = process.env.DEV_PORT || 3000
+const passport = require('./Auth'); // Import passport configuration from auth.js
+
+app.use(session({ secret: process.env.SESSION_SECRET , resave: false, saveUninitialized: true })); //telling express to use sesssion middleware [Secret used to sign the session cookie]
+app.use(passport.initialize()); //Telling express to use passport for authentication
+app.use(passport.session()); // Tell express to use passport.session() to support persistent login sessions (recommended).
 app.use(express.json({limit : '1mb'})); //telling that my webapp will be sending/recieving data in json format (limiting to 1MB)
 app.use(express.static(path.join(__dirname,"..","ClientSide","Static"))); //telling that my webapp will be using the files in the ClientSide folder (for the frontend
-require('dotenv').config() //including the .env file (for the API keys and DB Credentials)
+
 const {writeDB,readDB,updateDB,deleteDB} = require("./MongoOperations.js"); //including the MongoOperations.js file (for the DB operations)
 
 //------------------------------------------------------------------------------------------------------------------------------
 
+const port = process.env.DEV_PORT || 3000
 app.listen(port, () => {  
     console.log("Server Started at port " + process.env.DEV_PORT);    
 });
 
-app.get("/", (req, res) => {
+function isLoggedIn(req, res, next) { //Middleware to check if user is logged in
+  if (req.user) //check if user is authenticated
+    return next();
+  else
+    res.redirect("/"); //if not authenticated, redirect to login page
+}
+
+app.get("/", (req, res) => { //unprotected route
   res.sendFile(path.join(__dirname,"..","ClientSide","Knuth.html"));
 });
 
-app.get("/Coordinators",(req,res) => {
+app.get("/home",isLoggedIn,(req, res) => { //protected route
+  res.sendFile(path.join(__dirname,"..","ClientSide","home.html"));
+});
+
+app.get("/Coordinators",isLoggedIn,(req,res) => {
   res.sendFile(path.join(__dirname,"..","ClientSide","Coordinators.html"));
 })
 
-app.get("/coordinatorsList",(req,res) => {
+app.get("/coordinatorsList",isLoggedIn,(req,res) => {
   readDB("Main", "Coordinators", {}).then((coordinators) => {
       res.json(coordinators);
     }).catch((err) => {
       res.status(400).send("Cant' Read DB");
     })
 })
+
+app.get("/ConnectWithUs",isLoggedIn,(req,res) => {
+  res.sendFile(path.join(__dirname,"..","ClientSide","ConnectWithUs.html"));
+})
+
+app.get('/logout',isLoggedIn, (req, res, next) => {
+  res.clearCookie('connect.sid');
+  req.logout(function (err) {
+    req.session.destroy(function (err) { // destroys the session
+      res.redirect("/");
+    });
+  });
+});
+
+//to trigger google auth
+app.get("/auth/google", passport.authenticate('google', { scope: ['email', 'profile'], prompt: 'select_account' })); //route for google auth trigger
+
+//to handle the callback from google
+app.get("/auth/google/callback", passport.authenticate('google', {
+  successRedirect: "/home", //redirect to protected route if login successful
+  failureRedirect: "/auth/google/failure" //redirect to failure route if login fails
+}));
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+})
+
 
 // app.post("/create", (req, res) => {
 //     console.log(req.body);
