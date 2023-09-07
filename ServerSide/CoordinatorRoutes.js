5cbb4c3a3c31a8b86281514884d4320069704750
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-09-07 06:18:31 +0530

    simplified using templating

--- ServerSide/CoordinatorRoutes.js
+++ ServerSide/CoordinatorRoutes.js
@@ -1,21 +1,16 @@
 
 module.exports = (app) => {
-    
+
     const { readDB } = require("./MongoOperations");
     const { isLoggedIn } = require("./Middlewares.js");
     const path = require("path");
 
     //route that renders the coordinators page
     app.get("/coordinators", isLoggedIn, (req, res) => {
-        res.render(path.join(__dirname, "..", "ClientSide", "coordinators"), {page: "coordinators", emailTo: req.user.emails[0].value });
-    })
-    
-    //route that returns the list of coordinators fetched from the DB
-    app.get("/coordinatorsList", isLoggedIn, (req, res) => {
         readDB("Main", "Coordinators", {}).then((coordinators) => {
-            res.json(coordinators);
+            res.render(path.join(__dirname, "..", "ClientSide", "coordinators"), { page: "coordinators", emailTo: req.user.emails[0].value, coordinators: coordinators });
         }).catch((err) => {
-            res.status(400).send("Cant' Read DB");
+            res.render(path.join(__dirname, "..", "ClientSide", "coordinators"), { page: "coordinators", emailTo: req.user.emails[0].value, coordinators: [] });
         })
     })
 
