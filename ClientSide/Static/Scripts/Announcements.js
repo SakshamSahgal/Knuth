8476fa8292920897fc015b6bcb6ccdc31eed109e
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-09-23 22:38:48 +0530

    successfully build the upload system

--- ClientSide/Static/Scripts/Announcements.js
+++ ClientSide/Static/Scripts/Announcements.js
@@ -16,3 +16,38 @@ function closeIt(id) {
 function openIt(id) {
     document.getElementById(id).hidden = false;
   }
+
+  // Function to handle form submission
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = document.getElementById('myForm'); // Select form by its id
+    const formData = new FormData(form);
+
+    axios.post('/PostAnnouncements', formData)
+      .then(response => {
+        console.log('Response:', response.data);
+        // Handle success here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Handle errors here
+      });
+  }
+
+
+  // Add event listener to the form
+  const form = document.getElementById('myForm'); // Select form by its id
+  form.addEventListener('submit', (e) => {
+
+    e.preventDefault(); //preventing form refresh
+    
+    const formData = new FormData(form);
+    
+    axios.post("/PostAnnouncements", formData).then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      
+  });
---
+++ ServerSide/UploadImage/multer.js
@@ -0,0 +1,52 @@
+const multer = require("multer");
+    const fs = require("fs");
+
+    const storageEngine = multer.diskStorage({ //setting up multer storage engine
+        destination: 'uploads/',
+        filename: (req, file, cb) => {
+            cb(null, '-' + Date.now() + '-' + file.originalname ); //naming each file with the current date (we append original name at end to automatically add the file extension)
+        }
+    })
+
+    const upload = multer({ //middleware to upload files
+        storage: storageEngine,
+        limits: {
+            fileSize: parseInt(process.env.ImageSizeLimitInBytes) , //setting up file size limit to 1MB
+            files: parseInt(process.env.ImageUploadLimit)
+        },
+    })
+
+    const multerErrorHandling = (err, req, res, next) => { //middleware to handle multer errors
+
+        if (err instanceof multer.MulterError) {
+            return res.send(err.message);
+        } else {
+            next();
+        }
+    }
+
+    const TypeCheck = (req, res, next) => { //middleware to check the file type
+        
+        const allowedFileTypes = ['image/jpeg', 'image/png', 'image/gif']; //allowed file types
+        
+        var notallowed = false;
+
+        for (let i = 0; i < req.files.length; i++) {
+            if (!allowedFileTypes.includes(req.files[i].mimetype)) {
+                notallowed = true;
+                break;
+            }
+        }
+        
+        if (notallowed) {
+
+            for (let i = 0; i < req.files.length; i++) 
+                fs.unlinkSync(req.files[i].path); //deleting all the uploaded files
+            
+            return res.send("one or more File type is not supported");
+        }
+
+        next();
+    }
+
+    module.exports = {upload,multerErrorHandling,TypeCheck}
